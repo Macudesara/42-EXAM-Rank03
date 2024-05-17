c654b4dbd6ca91f9b5bbb02888d54ab1ace10b23
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-05-17 07:55:01 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -3,25 +3,25 @@
 
 char	*get_next_line(int fd)
 {
-	int		index;
+	int		i;
 	int		bytes;
 	char	*buffer;
-	char	character;
+	char	c;
 
 	if ((fd < 0) || (BUFFER_SIZE <= 0))
 		return (NULL);
 	index = 0;
-	bytes = read(fd, &character, 1);
+	bytes = read(fd, &c, 1);
 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	while (bytes > 0)
 	{
-		buffer[index++] = character;
-		if (character == '\n')
+		buffer[i++] = c;
+		if (c == '\n')
 			break ;
-		bytes = read(fd, &character, 1);
+		bytes = read(fd, &c, 1);
 	}
-	if ((bytes <= 0) && (index == 0))
+	if ((bytes <= 0) && (i == 0))
 		return (free(buffer), NULL);
-	buffer[index] = '\0';
+	buffer[i] = '\0';
 	return (buffer);
 }
